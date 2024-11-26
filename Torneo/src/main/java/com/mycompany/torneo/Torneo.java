d946ea8cd62f7db1737f395ed175d6c35420fc4d
Aluuu20 <alunni20cristiann@gmail.com>
2024-11-26 13:04:05 +0100

    commit mai

---
+++ Torneo/src/main/java/com/mycompany/torneo/Torneo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.torneo;
+
+/**
+ *
+ * @author alunn_bsg8exk
+ */
+public class Torneo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
